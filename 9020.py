f3aa3baa760b2e424a6462f83b35c1f649f4b75a
COkedat <47716620+COkedat@users.noreply.github.com>
2025-03-06 14:50:52 +0900

    골드바흐

---
+++ 9020.py
@@ -0,0 +1,55 @@
+"""문제
+1보다 큰 자연수 중에서  1과 자기 자신을 제외한 약수가 없는 자연수를 소수라고 한다. 예를 들어, 5는 1과 5를 제외한 약수가 없기 때문에 소수이다. 하지만, 6은 6 = 2 × 3 이기 때문에 소수가 아니다.
+
+골드바흐의 추측은 유명한 정수론의 미해결 문제로, 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다는 것이다. 이러한 수를 골드바흐 수라고 한다. 또, 짝수를 두 소수의 합으로 나타내는 표현을 그 수의 골드바흐 파티션이라고 한다. 예를 들면, 4 = 2 + 2, 6 = 3 + 3, 8 = 3 + 5, 10 = 5 + 5, 12 = 5 + 7, 14 = 3 + 11, 14 = 7 + 7이다. 10000보다 작거나 같은 모든 짝수 n에 대한 골드바흐 파티션은 존재한다.
+
+2보다 큰 짝수 n이 주어졌을 때, n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오. 만약 가능한 n의 골드바흐 파티션이 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고 짝수 n이 주어진다.
+
+출력
+각 테스트 케이스에 대해서 주어진 n의 골드바흐 파티션을 출력한다. 출력하는 소수는 작은 것부터 먼저 출력하며, 공백으로 구분한다."""
+import sys
+import math
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+import math
+
+n = 10000 # 2부터 10000까지의 모든 수에 대하여 소수 판별
+array = [True for i in range(n + 1)] # 처음엔 모든 수가 소수(True)인 것으로 초기화(0과 1은 제와)
+
+# 에라토스테네스의 체 알고리즘
+for i in range(2, int(math.sqrt(n)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인하며
+    if array[i] == True: # i가 소수인 경우(남은 수인 경우)
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
+
+
+def find_gold(n):
+    min = 9999
+    min_party = ()
+    for i in range(2, math.ceil(n/2)+1):
+        if not is_prime(i):
+            continue
+        if is_prime(n-i):
+            if(abs(i-(n-i))<min):
+                min = abs(i-(n-i))
+                min_party = f"{i} {n-i}"
+                if (min == 0):
+                    break
+    return min_party
+
+N = int(input())
+for _ in range(N):
+    tmp = int(sys.stdin.readline())
+    print(find_gold(tmp))
+
+
